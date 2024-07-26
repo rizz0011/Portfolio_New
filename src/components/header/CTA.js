77d4a573a4cb80ece207a4b0c6226b513e1ff3c5
Rizwan Ahmad Khan <ahmadkhanrizwan0@gmail.com>
2024-07-27 00:16:22 +0530

    add updated resume

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import FrontEnd_dev_RizwanAhmadKhan from '../../assets/FrontEnd_dev_RizwanAhmadKhan.pdf'
+import React_dev_3y_Rizwan from '../../assets/React_dev_3y_Rizwan.pdf'
 
 function CTA() {
     return (
         <div className='cta'>
-            <a href={FrontEnd_dev_RizwanAhmadKhan} download className='btn'>Download CV</a>
+            <a href={React_dev_3y_Rizwan} download className='btn'>Download CV</a>
             <a href='#contact' className='btn btn-primary'>Let's Talk</a>
         </div>
     )
