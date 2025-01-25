992b1a601beef18dc14abab8720ba5d9bf2e1f57
Rizwan Ahmad Khan <ahmadkhanrizwan0@gmail.com>
2025-01-25 17:05:30 +0530

    add upadted resume

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import React_dev_3y_Rizwan from '../../assets/Rizwan_Reactjs_3y.pdf'
+import React_dev_3y_Rizwan from '../../assets/Rizwan_Resume_ReactJs_3.5Y.pdf.pdf'
 
 function CTA() {
     return (
